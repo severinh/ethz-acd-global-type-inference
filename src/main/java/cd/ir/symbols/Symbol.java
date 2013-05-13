e2ab52ce3f3409a3ddfc6bf0d495e9cbbea3d818
Severin Heiniger <severinheiniger@gmail.com>
2013-05-13 21:36:36 +0200

    Add two sanity checks to the Symbol constructor, just in case.

--- src/main/java/cd/ir/symbols/Symbol.java
+++ src/main/java/cd/ir/symbols/Symbol.java
@@ -1,10 +1,16 @@
 package cd.ir.symbols;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkArgument;
+
 public abstract class Symbol {
 
 	public final String name;
 
 	protected Symbol(String name) {
+		checkNotNull(name);
+		checkArgument(!name.isEmpty());
+
 		this.name = name;
 	}
 
